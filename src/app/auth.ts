91e79ebeed31c9c4213d9e1b57859da02677ba24
Theophilus <isahtheophilus@gmail.com>
2023-12-18 03:23:23 +0100

    fix: 404 next auth error

--- src/app/auth.ts
+++ src/app/auth.ts
@@ -5,7 +5,7 @@ import {
 } from "next"
 import { getServerSession } from "next-auth"
 
-import { authOptions } from "./api/auth/[..nextauth]/options"
+import { authOptions } from "./api/auth/[...nextauth]/options"
 
 export function auth(
     ...args:
